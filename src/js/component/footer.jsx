b4e54ceb14338e6a307588d8998e3500c769dd0c
Pablo Méndez <pablo-mendez09@hotmail.com>
2022-08-25 13:48:36 +0000

    Mi primer commit

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,17 @@
+
+
+import React from "react";
+
+
+const Footer = () => {
+	return (
+    
+    <div className="text-center p-3" style={{backgroundColor: "black"}}>
+
+    <a className="text-white" href="https://mdbootstrap.com/">©Pablo Copyright:</a>
+  </div>
+	
+	);
+};
+
+export default Footer;
